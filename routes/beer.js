960a603fe919f1da96c6b1af80cea5b18892665d
Srijan R Shetty <srijan.shetty@gmail.com>
2014-12-09 14:44:27 +0530

    Refactored using a beer controller

--- routes/beer.js
+++ routes/beer.js
@@ -6,81 +6,22 @@ var router = express.Router();
 // Initiall dummy route
 router.get('/', function (req, res) {
     'use strict';
-    res.json({ message: 'hooray! It works'});
+    res.json({ message: 'Welcome to Beer Locker'});
 });
 
 // Initialize the Beer model
-var Beer = require('../models/beer');
+var beerController = require('../controllers/beer');
 
 // Create a route for /beers
-var beersRoute = router.route('/beers');
-beersRoute.post(function(req, res) {
-    var beer = new Beer();
+router.route('/beers')
+      .post(beerController.postBeers)
+      .get(beerController.getBeers);
 
-    beer.name = req.body.name;
-    beer.type = req.body.type;
-    beer.quantity = req.body.quantity;
-
-    beer.save(function(err) {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json({
-            message: 'Beer added to locker!',
-            data: beer
-        });
-    });
-});
-
-beersRoute.get(function(req, res) {
-    Beer.find(function(err, beers) {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(beers);
-    });
-});
-
-var beerRoute = router.route('/beers/:beer_id');
-beerRoute.get(function(req, res) {
-    Beer.findById(req.params.beer_id, function(err, beer) {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(beer);
-    });
-});
-
-beerRoute.put(function(req, res) {
-    Beer.findById(req.params.beer_id, function(err, beer) {
-        if (err) {
-            res.send(err);
-        }
-
-        beer.quantity = req.body.quantity;
-
-        beer.save(function(err) {
-            if (err) {
-                res.send(err);
-            }
-
-            res.json(beer);
-        });
-    });
-});
-
-beerRoute.delete(function(req, res) {
-    Beer.findByIdAndRemove(req.params.beer_id, function(err) {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json({ message: 'Beer succesfully removed' });
-    });
-});
+// Create routes for /beers/:beer_id
+router.route('/beers/:beer_id')
+      .get(beerController.getBeer)
+      .put(beerController.putBeer)
+      .delete(beerController.deleteBeer);
 
 module.exports = router;
 
